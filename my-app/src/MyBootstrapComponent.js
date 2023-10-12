975ff19bb864ea50712952f551491a252688333d
Berigny <134203992+Berigny@users.noreply.github.com>
2023-10-12 18:40:03 +1100

    Create MyBootstrapComponent.js

---
+++ my-app/src/MyBootstrapComponent.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+function MyBootstrapComponent() {
+  return <Button variant="primary">Click Me</Button>;
+}
+
+export default MyBootstrapComponent;
